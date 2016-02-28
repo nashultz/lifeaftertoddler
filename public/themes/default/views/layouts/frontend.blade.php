4be019ffcc5d1781fdb802f23d088a3bad9ef939
Nathon Shultz <nathon.shultz@systemsedgeonline.com>
2016-02-27 22:30:58 -0600

    changed

--- public/themes/default/views/layouts/frontend.blade.php
+++ public/themes/default/views/layouts/frontend.blade.php
@@ -8,6 +8,7 @@
     <title>Life After Toddler</title>
 
     <!-- Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Coming+Soon' rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/css?family=Lato:300,300italic" rel="stylesheet" type="text/css">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha256-3dkvEK0WLHRJ7/Csr0BZjAWxERc5WH7bdeUya2aXxdU= sha512-+L4yy6FRcDGbXJ9mPG8MT/3UCDzwR9gPeyFNMCtInsol++5m3bk2bXWKdZjvybmohrAsn3Ua5x8gfLnbE1YkOg==" crossorigin="anonymous">
 
